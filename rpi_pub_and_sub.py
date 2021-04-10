64cd9bf847d13ac23dd6cbf8aa92df3a092afe20
Larry Wang <larrywan@usc.edu>
2021-04-10 11:41:55 -0700

    First Trial

--- rpi_pub_and_sub.py
+++ rpi_pub_and_sub.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
             outfile.close()
             open("filename", "w").close()
             with lock:
-                temperature = grovepi.UltrasonicRead(sensor,'1.1')
+                temperature = grovepi.ultrasonicRead(sensor,'1.1')
                 time.sleep(0.5)
                 if(temperature>37):
                     setText_norefresh("Temerature High, Entry Denied")
