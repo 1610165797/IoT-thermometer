ad8e4ff81d89912addd883e9f8b68e99ab355150
Larry Wang <larrywan@usc.edu>
2021-04-11 03:12:10 -0700

    First Trial

--- webcam_cv3.py
+++ webcam_cv3.py
@@ -43,14 +43,17 @@ while True:
         minSize=(30, 30)
     )
     # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    if anterior != len(faces):
-        anterior = len(faces)
-        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+    if (len(faces)==0):
         client.publish("hospital/detected","False")
     else:
         client.publish("hospital/detected","True")
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    if anterior != len(faces):
+        anterior = len(faces)
+        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+
     # Display the resulting frame
     cv2.imshow('Video', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
